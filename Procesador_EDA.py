ebc89dfd8a90a3abb0041d9ab345f8dab8282f44
maria242506 <mpaubladelgado25@gmail.com>
2025-12-08 14:12:06 -0600

    Update Procesador_EDA
    Se le agragaron cambios al  codigo

--- Procesador_EDA.py
+++ Procesador_EDA.py
@@ -2,6 +2,76 @@
 import numpy as np
 import pandas as pd
 
+import csv
+
+#carga  y   limpieza    de  datos
+print("Usando módulo csv desde:", csv.__file__)
+
+
+
+ruta = r"C:\Users\Jesus\Downloads\datos_inec_convertidos.csv"
+
+df = pd.read_csv(ruta)
+
+print(df.head())
+print("Datos cargados correctamente ")
+
+
+print(df.isnull().sum())
+
+
+
+ruta = r"C:\Users\Jesus\Downloads\datos_inec_convertidos.csv"
+df = pd.read_csv(ruta)
+
+print("\nDatos cargados correctamente ✓")
+
+
+# 1. Identificar ID
+
+id_cols = ['ID_HOGAR', 'ID_VIVIENDA', 'id']
+print("\nColumnas ID detectadas:", id_cols)
+
+
+# 2. ID rellenar con 0
+
+df[id_cols] = df[id_cols].fillna(0)
+
+
+# 3. NUMÉRICAS
+
+cols_numericas = df.select_dtypes(include=['int64','float64']).columns.difference(id_cols)
+
+df[cols_numericas] = df[cols_numericas].fillna(df[cols_numericas].mean())
+
+
+# 4. CATEGÓRICAS
+
+cols_categoricas = df.select_dtypes(include=['object']).columns
+for col in cols_categoricas:
+    if df[col].isnull().any():
+        df[col] = df[col].fillna(df[col].mode()[0])
+
+
+# 5. Verificar
+
+print("\nNulos después de la limpieza:")
+print(df.isnull().sum())
+
+
+# 6. Guardar
+
+ruta_salida = r"C:\Users\Jesus\Downloads\datos_inec_convertidos_limpios.csv"
+df.to_csv(ruta_salida, index=False)
+
+print("\nArchivo limpio guardado en:")
+print(ruta_salida)
+
+
+print(df.isnull().sum().sum())
+
+
+
 #EDA
 class ProcesadorEDA:
     @property
